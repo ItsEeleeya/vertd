23386ae55f5859d3de5a348c5341ff5279c4787f
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2025-04-07 03:58:33 +0330

    clippy::single_match

--- server/src/services/websocket.rs
+++ server/src/services/websocket.rs
@@ -63,158 +63,153 @@ pub async fn websocket(req: HttpRequest, stream: web::Payload) -> Result<HttpRes
                 }
             };
 
-            match message {
-                Message::StartJob {
-                    token,
-                    job_id,
-                    to,
-                    speed,
-                } => {
-                    let Some(mut job) = ({
-                        let mut app_state = APP_STATE.lock().await;
-                        let job = app_state.jobs.get_mut(&job_id);
-                        let clone = job.as_ref().map(|j| (*j).clone());
-                        if let Some(job) = job {
-                            if job.completed {
-                                let message: String = Message::Error {
-                                    message: "job already completed".to_string(),
-                                }
-                                .into();
-                                session.text(message).await.unwrap();
-                                continue;
-                            }
-                            job.to = Some(to.clone());
-                        }
-                        clone
-                    }) else {
-                        let message: String = Message::Error {
-                            message: "job not found".to_string(),
-                        }
-                        .into();
-                        session.text(message).await.unwrap();
-                        continue;
-                    };
-
-                    if job.auth != token {
-                        let message: String = Message::Error {
-                            message: "invalid token".to_string(),
-                        }
-                        .into();
-                        session.text(message).await.unwrap();
-                        continue;
-                    }
-
-                    let Ok(from) = job.from.parse::<ConverterFormat>() else {
-                        let message: String = Message::Error {
-                            message: "invalid input format".to_string(),
-                        }
-                        .into();
-                        session.text(message).await.unwrap();
-                        continue;
-                    };
-
-                    let Ok(to) = to.parse::<ConverterFormat>() else {
-                        let message: String = Message::Error {
-                            message: "invalid output format".to_string(),
-                        }
-                        .into();
-                        session.text(message).await.unwrap();
-                        continue;
-                    };
-
-                    let converter = Converter::new(from, to, speed);
-
-                    let mut rx = match converter.convert(&mut job).await {
-                        Ok(rx) => rx,
-                        Err(e) => {
-                            let message: String = Message::Error {
-                                message: format!("failed to convert: {}", e),
-                            }
-                            .into();
-                            session.text(message).await.unwrap();
-                            continue;
-                        }
-                    };
-
-                    let mut logs = Vec::new();
-
-                    while let Some(update) = rx.recv().await {
-                        match update {
-                            ProgressUpdate::Error(err) => {
-                                logs.push(err);
-                            }
-                            _ => {
-                                let message: String = Message::ProgressUpdate(update).into();
-                                session.text(message).await.unwrap();
-                            }
-                        }
-                    }
-
+            if let Message::StartJob {
+                token,
+                job_id,
+                to,
+                speed,
+            } = message
+            {
+                let Some(mut job) = ({
                     let mut app_state = APP_STATE.lock().await;
-                    if let Some(job) = app_state.jobs.get_mut(&job_id) {
-                        job.completed = true;
-                    }
-                    drop(app_state);
-
-                    // check if output/{}.{} exists and isn't empty
-                    let is_empty = fs::metadata(&format!("output/{}.{}", job_id, to.to_string()))
-                        .await
-                        .map(|m| m.len() == 0)
-                        .unwrap_or(true);
-
-                    if is_empty {
-                        log::error!("job {} failed", job_id);
-                        let message: String = Message::Error {
-                            message: "oops -- your job failed! maddie has been notified :)"
-                                .to_string(),
-                        }
-                        .into();
-                        session.text(message).await.unwrap();
-
-                        let from = job.from.clone();
-                        let to = to.to_string().to_string();
-
-                        tokio::spawn(async move {
-                            if let Err(e) =
-                                handle_job_failure(job_id, from, to, logs.join("\n")).await
-                            {
-                                log::error!("failed to handle job failure: {}", e);
-                            }
-                        });
-                    } else {
-                        let message: String = Message::JobFinished { job_id }.into();
-                        session.text(message).await.unwrap();
-                    }
-
-                    tokio::spawn(async move {
-                        tokio::time::sleep(OUTPUT_LIFETIME).await;
-                        let mut app_state = APP_STATE.lock().await;
-                        app_state.jobs.remove(&job_id);
-                        drop(app_state);
-
-                        let path = format!("output/{}.{}", job_id, to.to_string());
-                        if let Err(e) = fs::remove_file(&path).await {
-                            if e.kind() != std::io::ErrorKind::NotFound {
-                                log::error!("failed to remove output file: {}", e);
-                            }
-                        }
-                    });
-
-                    match fs::remove_file(&format!("input/{}.{}", job.id, job.from)).await {
-                        Ok(_) => {}
-                        Err(e) => {
-                            error!("failed to remove input file: {}", e);
+                    let job = app_state.jobs.get_mut(&job_id);
+                    let clone = job.as_ref().map(|j| (*j).clone());
+                    if let Some(job) = job {
+                        if job.completed {
                             let message: String = Message::Error {
-                                message: format!("failed to remove input file: {}", e),
+                                message: "job already completed".to_string(),
                             }
                             .into();
                             session.text(message).await.unwrap();
                             continue;
                         }
-                    };
+                        job.to = Some(to.clone());
+                    }
+                    clone
+                }) else {
+                    let message: String = Message::Error {
+                        message: "job not found".to_string(),
+                    }
+                    .into();
+                    session.text(message).await.unwrap();
+                    continue;
+                };
+
+                if job.auth != token {
+                    let message: String = Message::Error {
+                        message: "invalid token".to_string(),
+                    }
+                    .into();
+                    session.text(message).await.unwrap();
+                    continue;
                 }
 
-                _ => {}
+                let Ok(from) = job.from.parse::<ConverterFormat>() else {
+                    let message: String = Message::Error {
+                        message: "invalid input format".to_string(),
+                    }
+                    .into();
+                    session.text(message).await.unwrap();
+                    continue;
+                };
+
+                let Ok(to) = to.parse::<ConverterFormat>() else {
+                    let message: String = Message::Error {
+                        message: "invalid output format".to_string(),
+                    }
+                    .into();
+                    session.text(message).await.unwrap();
+                    continue;
+                };
+
+                let converter = Converter::new(from, to, speed);
+
+                let mut rx = match converter.convert(&mut job).await {
+                    Ok(rx) => rx,
+                    Err(e) => {
+                        let message: String = Message::Error {
+                            message: format!("failed to convert: {}", e),
+                        }
+                        .into();
+                        session.text(message).await.unwrap();
+                        continue;
+                    }
+                };
+
+                let mut logs = Vec::new();
+
+                while let Some(update) = rx.recv().await {
+                    match update {
+                        ProgressUpdate::Error(err) => {
+                            logs.push(err);
+                        }
+                        _ => {
+                            let message: String = Message::ProgressUpdate(update).into();
+                            session.text(message).await.unwrap();
+                        }
+                    }
+                }
+
+                let mut app_state = APP_STATE.lock().await;
+                if let Some(job) = app_state.jobs.get_mut(&job_id) {
+                    job.completed = true;
+                }
+                drop(app_state);
+
+                // check if output/{}.{} exists and isn't empty
+                let is_empty = fs::metadata(&format!("output/{}.{}", job_id, to.to_string()))
+                    .await
+                    .map(|m| m.len() == 0)
+                    .unwrap_or(true);
+
+                if is_empty {
+                    log::error!("job {} failed", job_id);
+                    let message: String = Message::Error {
+                        message: "oops -- your job failed! maddie has been notified :)".to_string(),
+                    }
+                    .into();
+                    session.text(message).await.unwrap();
+
+                    let from = job.from.clone();
+                    let to = to.to_string().to_string();
+
+                    tokio::spawn(async move {
+                        if let Err(e) = handle_job_failure(job_id, from, to, logs.join("\n")).await
+                        {
+                            log::error!("failed to handle job failure: {}", e);
+                        }
+                    });
+                } else {
+                    let message: String = Message::JobFinished { job_id }.into();
+                    session.text(message).await.unwrap();
+                }
+
+                tokio::spawn(async move {
+                    tokio::time::sleep(OUTPUT_LIFETIME).await;
+                    let mut app_state = APP_STATE.lock().await;
+                    app_state.jobs.remove(&job_id);
+                    drop(app_state);
+
+                    let path = format!("output/{}.{}", job_id, to.to_string());
+                    if let Err(e) = fs::remove_file(&path).await {
+                        if e.kind() != std::io::ErrorKind::NotFound {
+                            log::error!("failed to remove output file: {}", e);
+                        }
+                    }
+                });
+
+                match fs::remove_file(&format!("input/{}.{}", job.id, job.from)).await {
+                    Ok(_) => {}
+                    Err(e) => {
+                        error!("failed to remove input file: {}", e);
+                        let message: String = Message::Error {
+                            message: format!("failed to remove input file: {}", e),
+                        }
+                        .into();
+                        session.text(message).await.unwrap();
+                        continue;
+                    }
+                };
             }
         }
     });
