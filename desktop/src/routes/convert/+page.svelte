f5ce119d529dbd48643d12e5962324eccdd30d6c
Ilya <47112191+ItsEeleeya@users.noreply.github.com>
2025-04-23 16:41:22 +0330

    Convert page layout

--- desktop/src/routes/convert/+page.svelte
+++ desktop/src/routes/convert/+page.svelte
@@ -1,7 +1,10 @@
-<div class="size-full p-2">
-	Convert
-	<div class="flex justify-between items-center">
-		<select name="pets" class="select bg-neutral-50/10 max-w-50">
+<div class="size-full px-2">
+	<div class="flex h-full bg-neutral-50/5 rounded-3xl p-5">Hello</div>
+
+	<div
+		class="fixed z-10 bottom-0 right-0 left-0 p-3 w-full flex justify-between items-center"
+	>
+		<select name="pets" class="select bg-neutral-50/10 max-w-30">
 			<option value="dog">.png</option>
 			<option value="cat">.jpeg</option>
 			<option value="hamster">.webp</option>
@@ -10,10 +13,9 @@
 			<option value="goldfish">.raw</option>
 		</select>
 		<div class="flex gap-2">
-      <button type="button" class="btn btn-error btn-square">X</button>
-			<button type="button" class="btn btn-neutral"
-      >Download all as .zip</button
-			>
+			<button type="button" class="btn btn-neutral">
+				Export all as .zip
+			</button>
 			<button type="button" class="btn btn-secondary">Convert all</button>
 		</div>
 	</div>
